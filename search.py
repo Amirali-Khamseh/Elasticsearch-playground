b644817642c3ee3c3d66b1116ed6e799f43eda24
Amir ali <amiralikhamseh01@gmail.com>
2025-12-27 12:42:56 +0100

    feat : Implementing Query DSL, multi_match to full-text search multiple fields

--- search.py
+++ search.py
@@ -29,9 +29,15 @@ class Search:
             operations.append({'index': {'_index': 'my_documents'}})
             operations.append(document)
         return self.es.bulk(operations=operations)
-        
+
     def reindex(self):
         self.create_index()
         with open('data.json', 'rt') as f:
             documents = json.loads(f.read())
-        return self.insert_documents(documents)    
+        return self.insert_documents(documents)
+
+    def search(self, **query_args):
+        return self.es.search(index='my_documents', **query_args)
+        
+    def retrieve_document(self, id):
+        return self.es.get(index='my_documents', id=id)
